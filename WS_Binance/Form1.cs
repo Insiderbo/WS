7f5283f7e49ee9ab00502cfd37bfa56c9070a84e
lan\insiderbo <91372@mail.ru>
2022-02-16 11:11:54 +0700

    Start

---
+++ WS_Binance/Form1.cs
@@ -0,0 +1,53 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Binance.Client.Websocket;
+using System.Net.WebSockets;
+
+namespace SocketAll
+{
+    public partial class Form1 : Form
+    {
+        public readonly string WsEndpoint = "wss://stream.binance.com:9443/stream?streams=ltcbtc@aggTrade/ethbtc@aggTrade";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async Task Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private async Task StartWebSocket()
+        {
+            var socket = new ClientWebSocket();
+            Uri serverUri = new Uri(WsEndpoint);
+
+            await socket.ConnectAsync(serverUri, CancellationToken.None);
+
+            while (socket.State == WebSocketState.Open)
+            {
+                ArraySegment<byte> bytesReceived = new ArraySegment<byte>(new byte[1024]);
+
+                WebSocketReceiveResult result = await socket.ReceiveAsync(bytesReceived, CancellationToken.None);
+                var line = Encoding.UTF8.GetString(bytesReceived.Array, 0, result.Count);
+                MessageBox.Show("Line: " + line);
+
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            StartWebSocket();
+        }
+    }
+}
